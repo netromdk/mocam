75cdb0c9371a4051ec4c795c99e8190173bd3a24
Morten Kristensen <msk@nullpointer.dk>
2014-03-08 20:07:55 +0100

    - Renamed snapshot to mac_snapper. - Created stub binary called snapshot in src/. - Created stub classes.

---
+++ src/CaptureSession.h
@@ -0,0 +1,9 @@
+#ifndef MOCAM_CAPTURE_SESSION_H
+#define MOCAM_CAPTURE_SESSION_H
+
+namespace mocam {
+  class CaptureSession {
+  };
+}
+
+#endif // MOCAM_CAPTURE_SESSION_H
---
+++ src/VideoDevice.h
@@ -0,0 +1,9 @@
+#ifndef MOCAM_VIDEO_DEVICE_H
+#define MOCAM_VIDEO_DEVICE_H
+
+namespace mocam {
+  class VideoDevice {
+  };
+}
+
+#endif // MOCAM_VIDEO_DEVICE_H
