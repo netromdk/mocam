104d49b1d1a62c75851908e0a15fd4233a89c643
Morten Kristensen <msk@nullpointer.dk>
2014-03-09 14:15:45 +0100

    Use SDK 10.9 with minimum comp with 10.8.

--- cmake/mac.cmake
+++ cmake/mac.cmake
@@ -1,7 +1,16 @@
+SET(SDK_MIN "10.8")
+SET(SDK "10.9")
+SET(DEV_SDK "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX${SDK}.sdk")
+
 ADD_DEFINITIONS(
   -DMAC
   -DGCC_VISIBILITY
+  -mmacosx-version-min=${SDK_MIN}
   )
+
+SET(CMAKE_OSX_SYSROOT ${DEV_SDK})
+
+MESSAGE("Using SDK: ${DEV_SDK}")
 
 FIND_LIBRARY(FOUNDATION_LIB Foundation)
 FIND_LIBRARY(AV_LIB AVFoundation)
