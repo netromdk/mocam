27ca307d7bfecdc66bda50ecbb661e7ee9b63410
Morten Kristensen <msk@nullpointer.dk>
2014-03-10 21:38:31 +0100

    Added deps on Qt5 and auto Moc'ing.

--- cmake/deps.cmake
+++ cmake/deps.cmake
@@ -1,2 +1,13 @@
+# Qt 5
+SET(CMAKE_AUTOMOC ON) # Moc when necessary.
+
+# As moc files are generated in the binary dir, tell CMake to always
+# look for includes there:
+SET(CMAKE_INCLUDE_CURRENT_DIR ON)
+
+FIND_PACKAGE(Qt5Core REQUIRED)
+FIND_PACKAGE(Qt5Gui REQUIRED)
+
+# OpenCV
 FIND_PACKAGE(OpenCV REQUIRED)
 SET(EXTRA_LIBS "${EXTRA_LIBS};${OpenCV_LIBS}")
