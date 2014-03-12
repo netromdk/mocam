dd52c5bdf9d7e3e0a7e34700a5e8e45df73f9dcb
Morten Kristensen <msk@nullpointer.dk>
2014-03-13 00:17:33 +0100

    First crude facial and eye detection algorithm.

--- src/faces.cpp
+++ src/faces.cpp
@@ -3,6 +3,7 @@
 #include <QString>
 #include <QCoreApplication>
 
+#include <vector>
 #include <memory>
 
 #include <opencv2/opencv.hpp>
@@ -45,14 +46,57 @@ int main(int argc, char **argv) {
     return -1;
   }
 
-  qDebug() << "Input:" << args->filename;
-
-  MatPtr mat = Util::imageToMat(args->filename);
-  if (mat == nullptr) {
+  MatPtr image = Util::imageToMat(args->filename);
+  if (image == nullptr) {
     qCritical() << "Invalid image!";
     return -1;
   }
-  qDebug() << "Loaded successfully..";
+  qDebug() << "Loaded image..";
+
+  cv::CascadeClassifier faceCas, eyesCas;
+  if (!faceCas.load("lbpcascade_frontalface.xml")) {
+    qCritical() << "Could not load facial cascade!";
+  }
+  if (!eyesCas.load("haarcascade_eye_tree_eyeglasses.xml")) {
+    qCritical() << "Could not load eyes cascade!";
+  }
+  qDebug() << "Loaded cascade files..";
+
+  // Make grayscaled version of the image, and equalize the histogram
+  // which normalizes the brightness and increases the contrast in the
+  // image.
+  cv::Mat grayImg;
+  cvtColor(*image.get(), grayImg, cv::COLOR_BGR2GRAY);
+  equalizeHist(grayImg, grayImg);
+  qDebug() << "Created grayscale image and equalized histogram..";
+
+  // Detect faces with scale factor 1.1, minimum 2 neighbors and
+  // minimum 80x80 face size.
+  qDebug() << "Detect faces..";
+  std::vector<cv::Rect> faces;
+  faceCas.detectMultiScale(grayImg, faces, 1.1, 2, 0, cv::Size(80, 80));
+
+  for (auto it = faces.begin(); it != faces.end(); ++it) {
+    const auto &face = *it;
+
+    QString msg = QString("  Face at (%1,%2) %3x%4")
+      .arg(face.x) .arg(face.y).arg(face.width).arg(face.height);
+    qDebug() << qPrintable(msg);
+
+    // Detect two eyes for each face with scale factor 1.1, 2
+    // min. neighbors and min size of 30x30.
+    cv::Mat facePart = grayImg(face);
+    std::vector<cv::Rect> eyes;
+    eyesCas.detectMultiScale(facePart, eyes, 1.1, 2, 0 | cv::CASCADE_SCALE_IMAGE,
+                             cv::Size(30, 30));
+    if (eyes.size() != 2) {
+      continue;
+    }
+    msg = QString("    Eyes at (%1,%2) %3x%4 and (%5,%6) %7x%8")
+      .arg(eyes[0].x).arg(eyes[0].y).arg(eyes[0].width).arg(eyes[0].height)
+      .arg(eyes[1].x).arg(eyes[1].y).arg(eyes[1].width).arg(eyes[1].height);
+    qDebug() << qPrintable(msg);
+  }
   
   return 0;
 }
